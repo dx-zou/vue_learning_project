5c4fc9136f01a1c2ad909af8228da0cedbcb4bbd
zoufeng <casafo@163.com>
2019-04-16 23:04:37 +0800

    v-model 父子组件传值

--- src/components/todo/todo.vue
+++ src/components/todo/todo.vue
@@ -1,32 +1,41 @@
 <template>
-    <div class="todo-container">
-        <ul>
-            <li 
-            v-for="todo in todolist"
-            :key="todo.id"
-            >
-                <slot name="todo" :todo="todo">{{todo.content}}</slot>
-            </li>
-        </ul>
-    </div>
+  <div class="todo-container">
+    <button @click="add">点击+1</button>
+    <p>通过v-model给子组件绑定数据,绕过单向数据流</p>
+    <h3>子组件的count：{{count}}</h3>
+    <ul>
+      <li v-for="todo in todolist" :key="todo.id">
+        <slot name="todo" :todo="todo">{{todo.content}}</slot>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'fnTodo',
-    props: {
-        todolist:{
-            type: Array,
-        }
+  name: "fnTodo",
+  props: {
+    todolist: {
+      type: Array
     },
-    data() {
-        return {
-           
-        }
-    },
-}
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      count: this.value
+    };
+  },
+  methods: {
+    add() {
+      this.count++;
+      this.$emit("input", this.count);
+    }
+  }
+};
 </script>
 
 <style lang="less">
-    
 </style>
--- src/views/todo/todolist.vue
+++ src/views/todo/todolist.vue
@@ -1,72 +1,62 @@
 <template>
-    <div class="todolist-container">
-        <div>
-            <h3>slot的新用法</h3>
-            <fn-todo :todolist="todolist">
-                <template v-slot:todo="{todo}">
-                    <span v-if="todo.isCompleted">x</span>
-                    {{todo.content}}
-                </template>
-            </fn-todo>
-        </div>
-        <div>
-            <h3>树形组件的使用</h3>
-            <el-row>
-                <el-col :span="12">
-                    <el-tree
-                    :data="data"
-                    :highlight-current="true"
-                    @node-expand="handleExpand"
-                    >
-                    </el-tree>
-                </el-col>
-                <el-col :span="12">
-                    <el-checkbox-group v-model="checkList">
-                        <el-checkbox 
-                        label="复选框 A"
-                        >
-                        </el-checkbox>
-                    </el-checkbox-group>
-                </el-col>
-            </el-row>
-        </div>
+  <div class="todolist-container">
+    <div>
+      <h3>slot的新用法</h3>
+      <fn-todo :todolist="todolist" v-model="count">
+        <template v-slot:todo="{todo}">
+          <span v-if="todo.isCompleted">x</span>
+          {{todo.content}}
+        </template>
+      </fn-todo>
+      <h3>父组件的count：{{count}}</h3>
     </div>
+    <div>
+      <!-- <h3>树形组件的使用</h3> -->
+      <!-- <el-row>
+        <el-col :span="12">
+          <el-tree :data="data" :highlight-current="true" @node-expand="handleExpand"></el-tree>
+        </el-col>
+        <el-col :span="12">
+          <el-checkbox-group v-model="checkList">
+            <el-checkbox label="复选框 A"></el-checkbox>
+          </el-checkbox-group>
+        </el-col>
+      </el-row>-->
+    </div>
+  </div>
 </template>
 <script>
 export default {
-    components: {
+  components: {},
+  data() {
+    return {
+      todolist: [
+        { id: 1, content: "111", isCompleted: false },
+        { id: 2, content: "222", isCompleted: false },
+        { id: 3, content: "333", isCompleted: false },
+        { id: 4, content: "444", isCompleted: true }
+      ],
+      data: [],
+      checkList: [],
+      count: 1
+    };
+  },
+  created() {
+    // this.getOrgTree()
+  },
+  methods: {
+    getOrgTree() {
+      this.$http({
+        method: "post",
+        url: "http://umapdev.dfmc.com.cn/smart/pc-org/getOrgTree"
+      }).then(res => {
+        this.data = res.data.data;
+      });
     },
-    data() {
-        return {
-             todolist: [
-                {id:1,content:'111',isCompleted: false},
-                {id:2,content:'222',isCompleted: false},
-                {id:3,content:'333',isCompleted: false},
-                {id:4,content:'444',isCompleted: true},
-            ],
-            data: [],
-            checkList:[]
-
-        }
-    },
-    created() {
-        // this.getOrgTree()
-    },
-    methods: {
-        getOrgTree() {
-            this.$http({
-                method: 'post',
-                url: 'http://umapdev.dfmc.com.cn/smart/pc-org/getOrgTree',
-            })
-            .then((res) => {
-                this.data = res.data.data
-                
-            })
-        },
-        //
-        handleExpand(data,node,obj) {
-            this.checkList = data.children
-        }
-    },
-}
+    //
+    handleExpand(data, node, obj) {
+      this.checkList = data.children;
+    }
+  }
+};
 </script>
