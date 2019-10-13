829905b52f034d4eda17bf0df031965a8150d4a3
zoufeng <casafo@163.com>
2019-10-13 09:58:08 +0800

    去除生产环境的log

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+let plugins = [];
+// 去除生产环境下的console.log
+if (process.env.NODE_ENV === "production") {
+  plugins.push("transform-remove-console");
+}
+
 module.exports = {
+  plugins: plugins,
   presets: ["@vue/app"]
 };
