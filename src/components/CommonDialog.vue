2b9881eafd3308517d4648ccdf67fda1420855fe
zoufeng <casafo@163.com>
2020-02-08 16:22:03 +0800

    优化公共弹窗组件

--- src/components/CommonDialog.vue
+++ src/components/CommonDialog.vue
@@ -1,18 +1,20 @@
 <template>
   <el-dialog
     :title="title"
-    :visible.sync="showDialog"
+    :visible.sync="visible"
     :width="width"
     :before-close="handleClose"
     :top="top"
-    center
+    :center="center"
     custom-class="common-dialog"
   >
     <slot></slot>
-    <span slot="footer" class="dialog-footer">
-      <el-button @click="close">取 消</el-button>
-      <el-button type="primary" @click="handleConfirm">确 定</el-button>
-    </span>
+    <div slot="footer">
+      <slot name="footer">
+        <el-button @click="close">取 消</el-button>
+        <el-button type="primary" @click="handleConfirm">确 定</el-button>
+      </slot>
+    </div>
   </el-dialog>
 </template>
 
@@ -23,25 +25,29 @@ export default {
     title: {
       default: ""
     },
-    showDialog: {
+    visible: {
       type: Boolean,
-      required: true
+      default: false
+    },
+    center: {
+      type: Boolean,
+      default: false
     },
     width: {
       type: String,
-      default: "30%"
+      default: "50%"
     },
     top: {
       type: String,
-      default: "15vh"
+      default: "10vh"
     }
   },
   methods: {
     handleClose() {
-      this.$emit("update:showDialog", false);
+      this.$emit("update:visible", false);
     },
     close() {
-      this.$emit("update:showDialog", false);
+      this.$emit("update:visible", false);
     },
     handleConfirm() {
       this.$emit("handleConfirm");
@@ -52,7 +58,9 @@ export default {
 
 <style lang="scss">
 .common-dialog {
-  font-size: 0.16rem;
-  border-radius: 0.1rem;
+  border-radius: 5px;
+  .el-dialog__body, .el-dialog__footer, .el-dialog__header{
+    padding: 10px 20px;
+  }
 }
 </style>
