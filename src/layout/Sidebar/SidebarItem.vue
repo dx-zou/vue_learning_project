e1b60bdb5e697d91ed13a3b404bdad5428b9960b
zoufeng <casafo@163.com>
2020-04-15 10:38:30 +0800

    优化菜单样式

--- src/layout/Sidebar/SidebarItem.vue
+++ src/layout/Sidebar/SidebarItem.vue
@@ -1,23 +1,21 @@
 <template>
-  <div>
-    <el-menu-item v-if="!item.children" :index="item.index">
-      <!-- <i :class="['iconfont', item.icon]"></i> -->
-      <router-link :to="{ name: item.title }">
-        {{ item.title }}
-      </router-link>
-    </el-menu-item>
-    <el-submenu v-else ref="subMenu" :index="item.index">
-      <template slot="title">
-        <svg-icon v-if="item.svg" :icon-class="item.svg" />
-        <span slot="title">{{ item.title }}</span>
-      </template>
-      <sidebar-item
-        v-for="child in item.children"
-        :key="child.id"
-        :item="child"
-      />
-    </el-submenu>
-  </div>
+  <el-menu-item v-if="!item.children" :index="item.index">
+    <!-- <i :class="['iconfont', item.icon]"></i> -->
+    <router-link :to="{ name: item.title }">
+      {{ item.title }}
+    </router-link>
+  </el-menu-item>
+  <el-submenu v-else ref="subMenu" :index="item.index">
+    <template slot="title">
+      <svg-icon v-if="item.svg" :icon-class="item.svg" />
+      <span slot="title">{{ item.title }}</span>
+    </template>
+    <sidebar-item
+      v-for="child in item.children"
+      :key="child.id"
+      :item="child"
+    />
+  </el-submenu>
 </template>
 
 <script>
