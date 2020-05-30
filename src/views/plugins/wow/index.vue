1a419db1091bd938f5cda9f3cd6324f65db83152
zoufeng <casafo@163.com>
2020-05-30 17:05:55 +0800

    refactor: 更新animate类名

--- src/views/plugins/wow/index.vue
+++ src/views/plugins/wow/index.vue
@@ -2,27 +2,26 @@
   <div class="wow-container">
     <div class="box">WOW.js animation</div>
     <div
-      class="box wow slideInRight"
+      class="box wow animate__slideInRight"
       data-wow-duration="2s"
       data-wow-delay="1s"
     ></div>
-    <div class="box wow slideInRight"></div>
-    <div class="box wow slideInLeft"></div>
-    <div class="box wow slideInLeft"></div>
-    <div class="box wow slideInLeft"></div>
-    <div class="box wow slideInLeft"></div>
-    <div class="box wow slideInLeft" data-wow-duration="2s"></div>
-    <div class="box wow slideInLeft"></div>
-    <div class="box wow slideInLeft"></div>
-    <div class="box wow slideInLeft"></div>
-    <div class="box wow slideInLeft"></div>
-    <div class="box wow slideInLeft"></div>
-    <div class="box wow slideInLeft"></div>
-    <div class="box wow slideInLeft"></div>
+    <div class="box wow animate__slideInRight"></div>
+    <div class="box wow animate__slideInLeft"></div>
+    <div class="box wow animate__slideInLeft"></div>
+    <div class="box wow animate__slideInLeft"></div>
+    <div class="box wow animate__slideInLeft"></div>
+    <div class="box wow animate__slideInLeft"></div>
+    <div class="box wow animate__slideInLeft"></div>
+    <div class="box wow animate__slideInLeft"></div>
+    <div class="box wow animate__slideInLeft"></div>
+    <div class="box wow animate__slideInLeft"></div>
+    <div class="box wow animate__slideInLeft"></div>
+    <div class="box wow animate__slideInLeft"></div>
+    <div class="box wow animate__slideInLeft"></div>
     <el-button type="primary" @click="toTop">回到顶部</el-button>
   </div>
 </template>
-
 <script>
 import { WOW } from "wowjs";
 export default {
@@ -36,7 +35,7 @@ export default {
     initWow() {
       const wow = new WOW({
         boxClass: "wow", // animated element css class (default is wow)
-        animateClass: "animated", // animation css class (default is animated)
+        animateClass: "animate__animated", // animation css class (default is animated)
         offset: 0, // distance to the element when triggering the animation (default is 0)
         mobile: true, // trigger animations on mobile devices (default is true)
         live: false, // act on asynchronously loaded content (default is true)
